8049675f42daa4d24aad448ac09431d5040e5214
thientantran <thientantm@gmail.com>
2022-11-22 10:01:47 +0700

    finish project

--- src/Project/Project.js
+++ src/Project/Project.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux'
-import { getTaskListApi } from '../Redux/actions/projectActions'
+import { getTaskListApi, addTaskApi, deleteTaskApi, checkTaskApi, rejectTaskApi } from '../Redux/actions/projectActions'
 import style from "./Project.css"
 export default function Project() {
     // lấy data từ redux về
@@ -20,22 +20,75 @@ export default function Project() {
         }
     }, [])
 
+    let [state, setState] = useState({
+        values: {
+            taskName: ''
+        },
+        errors: {
+            taskName: ''
+        }
+    });
+    const handleChange = (e) => {
+        let { value, name } = e.target;
+        let newValues = { ...state.values };
+
+        newValues = { ...newValues, [name]: value };
+
+        let newErrors = { ...state.errors };
+
+        let regexString = /^[a-z A-Z]+$/;
+
+        if (!regexString.test(value) || value.trim() === '') {
+            newErrors[name] = name + ' invalid !';
+        } else {
+            newErrors[name] = '';
+        }
+        setState({
+            ...state,
+            values: newValues,
+            errors: newErrors
+        })
+
+    }
+    const addTask = (e) => {
+        e.preventDefault(); //Dừng sự kiện submit form
+        // console.log(state.values.taskName);
+
+        //Xử lý nhận dữ liệu từ người dùng nhập => gọi action addTaskApi()
+        dispatch(addTaskApi(state.values.taskName))
+    }
+    //Hàm xử lý xóa task
+    const delTask = (taskName) => {
+        dispatch(deleteTaskApi(taskName))
+    }
+    const checkTask = (taskName) => {
+        dispatch(checkTaskApi(taskName))
+    }
+
+    //Xử lý reject task
+    const rejectTask = (taskName) => {
+        dispatch(rejectTaskApi(taskName));
+    }
+
     const renderTaskTodo = () => {
         return taskList.filter(item => !item.status).map((item, index) => {
             return <li key={index}>
                 <span>{item.taskName}</span>
                 <div className="buttons">
-                    <button className="remove">
+                    <button className="remove" type='button' onClick={() => {
+                        delTask(item.taskName)
+                    }}>
                         <i className="fa fa-trash-alt" />
                     </button>
-                    <button className="complete">
+                    <button type="button" className="complete" onClick={() => {
+                        checkTask(item.taskName)
+                    }}>
                         <i className="far fa-check-circle" />
                         <i className="fas fa-check-circle" />
                     </button>
                 </div>
             </li>
         })
-
     }
 
     const renderTaskDone = () => {
@@ -43,10 +96,14 @@ export default function Project() {
             return <li key={index}>
                 <span>{item.taskName}</span>
                 <div className="buttons">
-                    <button className="remove">
+                    <button className="remove" type='button' onClick={() => {
+                        delTask(item.taskName)
+                    }}>
                         <i className="fa fa-trash-alt" />
                     </button>
-                    <button className="complete">
+                    <button type="button" className="complete" onClick={() => {
+                        rejectTask(item.taskName)
+                    }}>
                         <i className="far fa-check-circle" />
                         <i className="fas fa-check-circle" />
                     </button>
@@ -61,18 +118,19 @@ export default function Project() {
                 <img src={require("./bg.png")} />
             </div>
             {/* <h2>hello!</h2> */}
-            <div className="card__body">
+            <form className="card__body" onSubmit={addTask}>
                 <div className="card__content">
                     <div className="card__title">
                         <h2>My Tasks</h2>
                         <p>September 9,2020</p>
                     </div>
                     <div className="card__add">
-                        <input id="newTask" type="text" placeholder="Enter an activity..." />
-                        <button id="addItem">
+                        <input id="newTask" name="taskName" type="text" placeholder="Enter an activity..." onChange={handleChange} />
+                        <button id="addItem" type="submit" onClick={addTask}>
                             <i className="fa fa-plus" />
                         </button>
                     </div>
+                    <div className="text-danger">{state.errors.taskName}</div>
                     <div className="card__todo">
                         {/* Uncompleted tasks */}
                         <ul className="todo" id="todo">
@@ -84,7 +142,7 @@ export default function Project() {
                         </ul>
                     </div>
                 </div>
-            </div>
+            </form>
         </div>
     )
 }
--- src/Redux/actions/projectActions.js
+++ src/Redux/actions/projectActions.js
@@ -21,6 +21,78 @@ export const getTaskListApi = () => {
         } catch (err) {
             console.log(err.response.data)
         }
+    }
+}
 
+export const addTaskApi = (taskName) => {
+    return dispatch => {
+        //Xử lý trước khi dispatch
+        let promise = Axios({
+            url: 'http://svcy.myclass.vn/api/ToDoList/AddTask',
+            method: 'POST',
+            data: { taskName: taskName }
+        });
+
+        //Xử lý thành công
+        promise.then(result => {
+            // alert(result.data);
+            dispatch(getTaskListApi());
+        })
+        //Xử lý thất bại
+        promise.catch(errors => {
+            alert(errors.response.data)
+
+        })
+    }
+}
+
+export const deleteTaskApi = (taskName) => {
+    return dispatch => {
+        let promise = Axios({
+            url: `http://svcy.myclass.vn/api/ToDoList/deleteTask?taskName=${taskName}`,
+            method: 'DELETE'
+        });
+
+        promise.then(result => {
+            //Sau khi thực hiện api gọi phương thức dispatchAction get TaskListApi để load lại task
+            dispatch(getTaskListApi())
+        });
+
+        promise.catch(errors => {
+            alert(errors.response.data)
+        })
+    }
+}
+export const checkTaskApi = (taskName) => {
+    return dispatch => {
+        let promise = Axios({
+            url: `http://svcy.myclass.vn/api/ToDoList/doneTask?taskName=${taskName}`,
+            method: 'PUT'
+        });
+
+        promise.then(res => {
+            dispatch(getTaskListApi())
+        });
+
+        promise.catch(err => {
+            alert(err.response.data);
+        })
+    }
+}
+export const rejectTaskApi = (taskName) => {
+    return dispatch => {
+        let promise = Axios({
+            url: `http://svcy.myclass.vn/api/ToDoList/rejectTask?taskName=${taskName}`,
+            method: 'PUT'
+        });
+
+        promise.then(res => {
+            // alert(res.data);
+            dispatch(getTaskListApi());
+        });
+
+        promise.catch(err => {
+            alert(err.response.data);
+        })
     }
 }
